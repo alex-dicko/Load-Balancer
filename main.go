d304e78f56b22f62e3dc8e293cd08837853f48e2
alex-dicko <alexdicko2007@icloud.com>
2026-06-20 20:52:25 +0100

    intial

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+	"sync/atomic"
+)
+
+type Backend struct {
+	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.Lock()
+	b.Alive = alive
+	b.mux.Unlock()
+}
+
+func (b *Backend) IsAlive() (alive bool) {
+	b.mux.RLock()
+	alive = b.Alive
+	b.mux.RUnlock()
+	return
+}
+
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
+
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+}
+
+func (s *ServerPool) GetNextPeer() *Backend {
+	next := s.NextIndex()
+	l := len(s.backends) + next
+	for i := next; i < l; i++ {
+		idx := i % len(s.backends)
+		if s.backends[idx].IsAlive() {
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(idx)) // mark the current one
+			}
+			return s.backends[idx]
+		}
+	}
+	return nil
+}
+
+func startBackend(port string) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from backend on port %s\n", port)
+	})
+	http.ListenAndServe(":"+port, mux)
+}
+
+func lb(w http.ResponseWriter, r *http.Request) {
+	peer := serverPool.GetNextPeer()
+	if peer != nil {
+		peer.ReverseProxy.ServeHTTP(w, r)
+		return
+	}
+	http.Error(w, "Service not available", http.StatusServiceUnavailable)
+}
+
+func main() {
+	go startBackend("8001")
+	go startBackend("8002")
+	go startBackend("8003")
+
+	u, _ := url.Parse("http://localhost:8001")
+	rp := httputil.NewSingleHostReverseProxy(u)
+
+	http.ListenAndServe(":8080", rp)
+}
